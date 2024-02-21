552773c7c5f8bcfcb85d67d5dd3db8007791f0af
GloomsfieldRooster <rooster@gloomsfield.co>
2024-02-21 14:15:19 -0800

    Implement chatSourceGetName

--- src/chatSource.cpp
+++ src/chatSource.cpp
@@ -1,14 +1,21 @@
 #include "chatSource.h"
 
+static const char * chatSourceGetName(void * unused)
+{
+    UNUSED_PARAMETER(unused);
+
+    return obs_module_text("ChatSource");
+}
+
 struct obs_source_info chatSourceInfo
 {
     .id = "p5_chat_source",
     .type = OBS_SOURCE_TYPE_INPUT,
     .output_flags = OBS_SOURCE_VIDEO,
+    .get_name = chatSourceGetName,           // static const char * (void * unused)
     .icon_type = OBS_ICON_TYPE_TEXT,
     /* To be implemented                        Signatures
     -------------------------------------------------------------------------------
-    .get_name = chatSourceGetName,           // static const char * (void * unused)
     .create = chatSourceCreate,              // static void * (obs_data_t * settings, obs_source_t * source)
     .get_width = chatSourceGetWidth,         // static uint32_t (void * data)
     .get_height = chatSourceGetHeight,       // static uint32_t (void * data)
