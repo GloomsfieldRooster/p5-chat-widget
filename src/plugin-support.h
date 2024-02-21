58b852bd25ebb469e231372846c8d586f326dbb6
GloomsfieldRooster <rooster@gloomsfield.co>
2024-02-21 12:10:18 -0800

    Update copyright information

--- src/plugin-support.h
+++ src/plugin-support.h
@@ -1,6 +1,6 @@
 /*
-Plugin Name
-Copyright (C) <Year> <Developer> <Email Address>
+P5 Chat Widget
+Copyright (C) 2024 | Amory Acosta | rooster@gloomsfield.co
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
